171013c732a6700a0adf7cce77fa2919b0ca5af1
Lucas Risaro <lrisaro@cys.com.ar>
2021-07-09 21:25:18 -0300

    Elimino archivo de targets y test viejos, actualizo los archivos de random forest y xgboost ahora se importan las funciones desde el archivo .py

--- parte_2/preprocessing.py
+++ parte_2/preprocessing.py
@@ -1,3 +1,6 @@
+import pandas as pd
+from sklearn import preprocessing
+
 #Funcion que devuelve un 1 si esa row posee esta combinacion en especifico de valores en sus columnas "trabajo" y "rol_familiar_registrado"
 def set_value_row_casado_trabajo(row):
     if (row.rol_familiar_registrado == "casado" and (row.trabajo == "profesional_especializado" or row.trabajo == "directivo_gerente" )):
@@ -5,6 +8,10 @@ def set_value_row_casado_trabajo(row):
     else:
       return 0
 
+#Aplica one hot encoding
+def apply_one_hot_encoding(df):  
+    return pd.get_dummies(df, drop_first=True, columns=["categoria_de_trabajo", "educacion_alcanzada", "estado_marital", "religion", "rol_familiar_registrado", "trabajo"])
+    
 def feature_engineering_xg_rf(df):
 
     #Creo columna opera_en_bolsa que indica si opero en bolsa o no
@@ -22,8 +29,8 @@ def feature_engineering_xg_rf(df):
     #Dropeo columnas que no considero necesarias para la prediccion
     df.drop(columns = ["edad", "horas_trabajo_registradas", "barrio", "genero", "ganancia_perdida_declarada_bolsa_argentina", "anios_estudiados"], inplace=True)
 
-    #Aplico one hot enconding a estas columnas en especifico
-    df = pd.get_dummies(df, drop_first=True, columns=["categoria_de_trabajo", "educacion_alcanzada", "estado_marital", "religion", "rol_familiar_registrado", "trabajo"])
+    #Aplico one hot encoding a estas columnas en especifico  
+    df = apply_one_hot_encoding(df)
 
     #Borro una columna de cada uno de los one hot creados
     df.drop(columns = ["categoria_de_trabajo_empleado_municipal", "educacion_alcanzada_1_anio", "estado_marital_matrimonio_civil", "religion_budismo", "rol_familiar_registrado_con_hijos", "trabajo_ejercito"], inplace=True)
